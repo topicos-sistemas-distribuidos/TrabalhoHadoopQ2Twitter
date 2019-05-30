a372de73e5e9085b906495ebe863a31b500e8c49
Juarez <juarezlmfilho@gmail.com>
2019-05-30 15:46:27 -0300

    Solução para a questão 2 - item b) com hora, dia e mes

---
+++ src/br/ufc/mdcc/cc/hadoop/HoraDiaMes.java
@@ -0,0 +1,38 @@
+package br.ufc.mdcc.cc.hadoop;
+
+public class HoraDiaMes {
+	
+	private Integer hora;
+	private Integer dia;
+	private Integer mes;
+	
+		
+	public HoraDiaMes(Integer hora, Integer dia, Integer mes) {
+		super();
+		this.hora = hora;
+		this.dia = dia;
+		this.mes = mes;
+	}
+	
+	public Integer getHora() {
+		return hora;
+	}
+	public void setHora(Integer hora) {
+		this.hora = hora;
+	}
+	public Integer getDia() {
+		return dia;
+	}
+	public void setDia(Integer dia) {
+		this.dia = dia;
+	}
+	public Integer getMes() {
+		return mes;
+	}
+	public void setMes(Integer Mes) {
+		this.mes = mes;
+	}
+	
+	
+
+}
